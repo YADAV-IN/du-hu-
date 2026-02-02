a3aadc6c800794c9defbdfdc92912df2c2f2ef8f
VIPNO1 <102744176+YADAV-IN@users.noreply.github.com>
2026-02-02 11:14:40 +0000

    Expose Vercel app/handler

--- hello_world/wsgi.py
+++ hello_world/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_world.settings")
 
 application = get_wsgi_application()
+
+# Vercel expects `app` or `handler`
+app = application
+handler = application
